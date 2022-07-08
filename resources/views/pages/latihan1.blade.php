54abe7cdd9812736522e7776110487320265abed
rizkiiiiii <muhamad_141@smkassalaambandung.sch.id>
2022-07-08 11:34:30 +0700

    Belajar Laravel

---
+++ resources/views/pages/latihan1.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <center>
+        Hallo Nama Saya <b>{{$nama}}</b><br>
+        Umur saya <b>{{$umur}}</b><br>
+        alamat saya <b>{{$alamat}}</b><br>
+        Kota Asal saya <b>{{$kota}}</b><br>
+        Hobby saya <b>{{$hobby}}</b><br>
+        </center>
+    </body>
+    </html>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//route basic
+Route::get('about', function() {
+    return view ('tentang');
+});
+
+Route::get('profile', function () {
+    $nama = "Rizki";
+    return view('pages/profile' , compact('nama'));
+});
+
+Route::get('latihan1', function () {
+    $nama = "Rizki";
+    $umur = "17 tahun 4bulan 8hari";
+    $alamat = "Jln Cibaduyut Raya";
+    $kota ="Bandung";
+    $hobby = "Tidur";
+    return view('pages/latihan1' , compact('nama','umur','alamat','kota','hobby'));
+});
+// ROUTE PARAMETER
+
+Route::get('biodata/{nama}', function ($a) {
+    return view('pages/biodata' , compact('a'));
+});
+Route::get('makanan/{makanan}/{minuman}/{harga}', function ($makanan,$minuman,$harga) {
+    return view('pages/makanan' , compact('makanan','minuman','harga'));
+});
+
+//ROUTE OPTIONAL PARAMETER
+
+Route::get('pesan/{menu?}',function ($a = "-"){
+    return view('pages/pesan',compact('a'));
+
+});
